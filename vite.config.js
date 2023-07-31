5f76986691835a9576210171dcaf561a1fa7e2f3
Ofek Abramovitch <ofek301156@gmail.com>
2023-07-31 21:10:15 +0300

    edited base in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Mr.Bitcoin-Vue/',
+  publicPath: process.env.NODE_ENV === "production" ? "/Mr.Bitcoin-Vue/" : "/",
   plugins: [
     vue(),
   ],
